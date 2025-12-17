2e764c4d2afa0e25a2787d994dd0ddb66b6fc844
drwhoisthat-pro <christian.a.blank@gmail.com>
2025-12-17 17:20:58 +0100

    Update 70_ollama.py

--- 03_LLMs/70_ollama.py
+++ 03_LLMs/70_ollama.py
@@ -15,7 +15,7 @@ from langchain_community.llms import Ollama
 llm = Ollama(model="gemma2:2b")
 
 # %%
-response = llm.invoke("What is an LLM?")
+response = llm.invoke("What is a LLM?")
 
 # %%
 response
