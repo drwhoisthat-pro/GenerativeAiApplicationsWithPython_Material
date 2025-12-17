4798923959a2bb3884bde2bf5823216018530a19
drwhoisthat-pro <christian.a.blank@gmail.com>
2025-12-17 16:58:45 +0100

    Update 10_text_summarization.py

--- 02_PreTrainedNetworks/10_text_summarization.py
+++ 02_PreTrainedNetworks/10_text_summarization.py
@@ -1,6 +1,7 @@
 #%% packages
 from transformers import pipeline
 from langchain_community.document_loaders import ArxivLoader
+
 #%% model selection
 task = "summarization"
 model = "sshleifer/distilbart-cnn-12-6"
@@ -13,11 +14,12 @@ docs = loader.load()
 
 # %% Data Preparation
 article_text = docs[0].page_content
-# %%
+
+# %% Run the summarizer
 result = summarizer(article_text[:2000], min_length=20, max_length=80, do_sample=False)
 result[0]['summary_text']
+
 # %% number of characters
 len(result[0]['summary_text'].split(' '))
-
 
 # %%
